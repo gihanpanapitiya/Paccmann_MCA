c6d2f69fb05eb2210f0cda85745a6392cd74cc7a
priyanka9991 <pvasanthakumari@anl.gov>
2022-11-16 11:30:35 -0600

    update script

--- infer.sh
+++ infer.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Launches model for inferencing from within the container
-# TODO: define what args can be passed
 
-CANDLE_DATA_DIR=$1
-export CANDLE_DATA_DIR=$CANDLE_DATA_DIR
 
 ####################################################################
 ## Exceute inference script for your model with the CMD arguments ##
 ####################################################################
+infer="/usr/local/Paccmann_MCA/infer.py"
+
+
+CUDA_VISIBLE_DEVICES=$1
+CANDLE_DATA_DIR=$2
+
+CMD="python3 ${infer}"
+
+
+echo "using container "
+echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
+echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
+echo "running command ${CMD}"
+
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
