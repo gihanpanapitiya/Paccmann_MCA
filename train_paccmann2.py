b247376d8bfce591ecf800e82ff1f7d5c6a49b82
priyanka9991 <pvasanthakumari@anl.gov>
2022-12-08 15:15:29 -0600

    Add validation

--- train_paccmann2.py
+++ train_paccmann2.py
@@ -16,6 +16,9 @@ from paccmann_predictor.utils.hyperparams import OPTIMIZER_FACTORY
 from paccmann_predictor.utils.loss_functions import pearsonr
 from paccmann_predictor.utils.utils import get_device
 import candle
+import pandas as pd
+from scipy.stats import spearmanr
+import sklearn
 
 # setup logging
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -24,9 +27,12 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'train_sensitivity_filepath', type=str,
-    help='Path to the drug sensitivity (IC50) data.'
+    help='Path to the train drug sensitivity (IC50) data.'
 )
-
+parser.add_argument(
+    'val_sensitivity_filepath', type=str,
+    help='Path to the validation drug sensitivity (IC50) data.'
+)
 parser.add_argument(
     'gep_filepath', type=str,
     help='Path to the gene expression profile data.'
@@ -61,7 +67,7 @@ parser.add_argument(
 
 
 def main(
-    train_sensitivity_filepath,  gep_filepath,
+    train_sensitivity_filepath, val_sensitivity_filepath, gep_filepath,
     smi_filepath, gene_filepath, smiles_language_filepath, output_dir,
     model_name, model_params
 ):
@@ -73,6 +79,7 @@ def main(
     # Create model directory and dump files
     model_dir = os.path.join(output_dir, model_name)
     os.makedirs(os.path.join(model_dir, 'weights'), exist_ok=True)
+    os.makedirs(os.path.join(model_dir, 'results'), exist_ok=True)
     #with open(os.path.join(model_dir, 'model_params.json'), 'w') as fp:
    	 #json.dump(dict(params), fp, indent=4)
 
@@ -128,6 +135,50 @@ def main(
 
     logger.info(
         f'Training dataset has {len(train_dataset)} samples.'
+    )
+
+
+    val_dataset = DrugSensitivityDataset(
+        drug_sensitivity_filepath=val_sensitivity_filepath,
+        smi_filepath=smi_filepath,
+        gene_expression_filepath=gep_filepath,
+        smiles_language=smiles_language,
+        gene_list=gene_list,
+        drug_sensitivity_min_max=params.get('drug_sensitivity_min_max', True),
+        drug_sensitivity_processing_parameters=params.get(
+            'drug_sensitivity_processing_parameters', {}
+        ),
+        augment=params.get('augment_test_smiles', False),
+        canonical=params.get('canonical', False),
+        kekulize=params.get('kekulize', False),
+        all_bonds_explicit=params.get('all_bonds_explicit', False),
+        all_hs_explicit=params.get('all_hs_explicit', False),
+        randomize=params.get('randomize', False),
+        remove_bonddir=params.get('remove_bonddir', False),
+        remove_chirality=params.get('remove_chirality', False),
+        selfies=params.get('selfies', False),
+        add_start_and_stop=params.get('smiles_start_stop_token', True),
+        padding_length=params.get('smiles_padding_length', None),
+        gene_expression_standardize=params.get(
+            'gene_expression_standardize', True
+        ),
+        gene_expression_min_max=params.get('gene_expression_min_max', False),
+        gene_expression_processing_parameters=params.get(
+            'gene_expression_processing_parameters', {}
+        ),
+        device=torch.device(params.get('dataset_device', 'cpu')),
+        backend='eager'
+    )
+    val_loader = torch.utils.data.DataLoader(
+        dataset=val_dataset,
+        batch_size=params['batch_size'],
+        shuffle=True,
+        drop_last=True,
+        num_workers=params.get('num_workers', 0)
+    )
+    logger.info(
+        f'Validation set has '
+        f'{len(val_dataset)}.'
     )
 
     device = get_device()
@@ -212,12 +263,64 @@ def main(
     logger.info('Done with training, models saved, shutting down.')
 
 
+    # Measure validation performance
+    model.eval()
+    with torch.no_grad():
+        val_loss = 0
+        predictions = []
+        labels = []
+        for ind, (smiles, gep, y) in enumerate(val_loader):
+            y_hat, pred_dict = model(
+                torch.squeeze(smiles.to(device)), gep.to(device)
+            )
+            predictions.append(y_hat)
+            labels.append(y)
+            loss = model.loss(y_hat, y.to(device))
+            val_loss += loss.item()
+
+    predictions = np.array(
+        [p.cpu() for preds in predictions for p in preds]
+    ,dtype = np.float )
+    predictions = predictions[0:len(predictions)]
+    labels = np.array([l.cpu() for label in labels for l in label],dtype = np.float)
+    labels = labels[0:len(labels)]
+    val_pearson_a = pearsonr(torch.Tensor(predictions), torch.Tensor(labels))
+    val_spearman_a = spearmanr(labels, predictions)[0]
+    mean_absolute_error = sklearn.metrics.mean_absolute_error(y_true=labels, y_pred=predictions)
+    r2 = sklearn.metrics.r2_score(y_true=labels, y_pred=predictions)
+    val_rmse_a = np.sqrt(np.mean((predictions - labels)**2))
+    val_loss_a = val_loss / len(val_loader)
+    #Creating a dictionary with the scores
+    scores = {}
+    scores['r2'] = r2
+    scores['mean_absolute_error'] = mean_absolute_error
+    scores['spearmanr'] = val_spearman_a
+    scores['pearsonr'] = val_pearson_a.cpu().detach().numpy().tolist()
+    logger.info(
+        f"\t **** VALIDATION  **** "
+        f"loss: {val_loss_a:.5f}, "
+        f"Pearson: {val_pearson_a:.3f}, "
+        f"RMSE: {val_rmse_a:.3f}"
+    )
+    # Save scores and final preds
+    save_dir = str(model_dir+'/results/val_results.json')
+    with open(save_dir, 'w') as fp:
+        json.dump(scores, fp)
+    pred = pd.DataFrame({"True": labels, "Pred": predictions}).reset_index()
+    te_df1 = val_loader.dataset.drug_sensitivity_df[['drug','cell_line', 'IC50']].reset_index()
+    te_df = te_df1.rename(columns={'drug': 'DrugID', 'cell_line': 'CancID', 'IC50':'IC50'})
+    pred = pd.concat([te_df, pred], axis=1)
+    pred['IC50'] = ((pred['IC50']*1000).apply(np.round))/1000
+    pred['True'] = ((pred['True']*1000).apply(np.round))/1000
+    pred_fname = str(model_dir+'/results/val_pred.csv')
+    pred.to_csv(pred_fname, index=False)
+
 if __name__ == '__main__':
     # parse arguments
     args = parser.parse_args()
     # run the training
     main(
-        args.train_sensitivity_filepath, 
+        args.train_sensitivity_filepath, args.val_sensitivity_filepath,
         args.gep_filepath, args.smi_filepath, args.gene_filepath,
         args.smiles_language_filepath, args.output_dir, 
         args.model_name, args.model_params
