f68c43967cb058e89d2fcf27820b5345891a3cb3
brettin <brettin@.anl.gov>
2022-12-08 13:42:43 +0000

    implementation to support supervisor requirements

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-#############################################################################################################
-### THIS IS A TEMPLATE FILE. SUBSTITUTE '???' ENTRIES WITH THE APPROPRiATE INFORMATION FOR YOUR CONTAINER ###
-#############################################################################################################
+  
+#########################################################################
+### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
+#########################################################################
 
 
 # arg 1 CUDA_VISIBLE_DEVICES
@@ -12,25 +12,39 @@
 ### Path to your CANDLEized model's main Python script###
 CANDLE_MODEL=/usr/local/Paccmann_MCA/train.py
 
-# Reading command line arguments
-CUDA_VISIBLE_DEVICES=$1
-CANDLE_DATA_DIR=$2
-CANDLE_CONFIG=$3
+if [ $# -lt 2 ] ; then
+        echo "Illegal number of parameters"
+        echo "CUDA_VISIBLE_DEVICES and CANDLE_DATA_DIR are required"
+        exit -1
+fi
 
-# Check if CANDLE_CONFIG exists if user passed this parameter and prepare 
-if [ ! -z "${CANDLE_CONFIG}" ]; then
-        if [ ! -f "$CANDLE_CONFIG" ]; then
-	    echo "Usage: train.sh CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR CANDLE_CONFIG"
-			exit -1
-		else
-			CMD="python3 ${CANDLE_MODEL} --config_file ${CANDLE_CONFIG}"
+if [ $# -eq 2 ] ; then
+        CUDA_VISIBLE_DEVICES=$1 ; shift
+        CANDLE_DATA_DIR=$1 ; shift
+        CMD="python ${CANDLE_MODEL}"
+        echo "CMD = $CMD"
+
+elif [ $# -ge 3 ] ; then
+        CUDA_VISIBLE_DEVICES=$1 ; shift
+        CANDLE_DATA_DIR=$1 ; shift
+
+        # if original $3 is a file, set candle_config and passthrough $@
+        if [ -f $1 ] ; then
+		echo "$1 is a file"
+                CANDLE_CONFIG=$1 ; shift
+                CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG $@"
+                echo "CMD = $CMD $@"
+
+        # else passthrough $@
+        else
+		echo "$1 is not a file"
+                CMD="python ${CANDLE_MODEL} $@"
+                echo "CMD = $CMD"
+
         fi
-else
-	CMD="python3 ${CANDLE_MODEL}"
 fi
 
 # Display runtime arguments
-echo "using container "
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
 echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
 echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
@@ -39,3 +53,4 @@ echo "running command ${CMD}"
 # Set up environmental variables and execute model
 source /opt/conda/bin/activate /usr/local/conda_envs/Paccmann_MCA
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
+
