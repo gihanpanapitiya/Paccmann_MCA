dd918012ccb837481709ac70e8ecf8cd9899e858
priyanka9991 <pvasanthakumari@anl.gov>
2023-05-02 11:02:18 -0500

    Add file

---
+++ preprocess.py
@@ -0,0 +1,144 @@
+import candle
+import os
+from paccmannmca_baseline_pytorch import main
+import json
+
+
+# This should be set outside as a user environment variable
+#os.environ['CANDLE_DATA_DIR'] = '/homes/brettin/Singularity/workspace/data_dir/'
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+
+additional_definitions = [
+    {'name': 'gep_filepath',
+     'type': str,
+     'help': 'Path to the gene expression profile data.'
+     },
+    {'name': 'smi_filepath',
+     'type': str,
+     'help': 'Path to the SMILES data.'
+     },
+    {'name': 'gene_filepath',
+     'type': str,
+     'help': 'Path to a pickle object containing list of genes.'
+     },
+    {'name': 'smiles_language_filepath',
+     'type': str,
+     'help': 'Path to a pickle object a SMILES language object.'
+     },
+
+    {'name': 'drug_sensitivity_min_max',
+     'type': bool,
+     'help': '.....'
+     },
+    {'name': 'gene_expression_standardize',
+     'type': bool,
+     'help': 'Do you want to standardize gene expression data?'
+     },
+    {'name': 'augment_smiles',
+     'type': bool,
+     'help': 'Do you want to augment smiles data?'
+     },
+    {'name': 'smiles_start_stop_token',
+     'type': bool,
+     'help': '.....'
+     },
+    {'name': 'number_of_genes',
+     'type': int,
+     'help': 'Number of selected genes'
+     },
+    {'name': 'smiles_padding_length',
+     'type': int,
+     'help': 'Padding length for smiles strings'
+     },
+    {'name': 'filters',
+     'type': list,
+     'help': 'Size of filters'
+     },
+    {'name': 'multiheads',
+     'type': list,
+     'help': 'Size of multiheads for attention layer'
+     },
+    {'name': 'smiles_embedding_size',
+     'type': int,
+     'help': 'Size of smiles embedding'
+     },
+    {'name': 'kernel_sizes',
+     'type': list,
+     'help': 'Size of the kernels'
+     },
+    {'name': 'smiles_attention_size',
+     'type': int,
+     'help': 'Size of smiles attention'
+     },
+    {'name': 'embed_scale_grad',
+     'type': bool,
+     'help': '.....'
+     },
+    {'name': 'final_activation',
+     'type': bool,
+     'help': 'Is there a final activation?'
+     },
+    {'name': 'gene_to_dense',
+     'type': bool,
+     'help': '.....'
+     },
+    {'name': 'smiles_vocabulary_size',
+     'type': int,
+     'help': 'Size of smiles vocabulary'
+     },
+    {'name': 'number_of_parameters',
+     'type': int,
+     'help': 'Number of parameters'
+     },
+    {'name': 'drug_sensitivity_processing_parameters',
+     'type': dict,
+     'help': 'Parameters for drug sensitivity processing'
+     },
+    {'name': 'gene_expression_processing_parameters',
+     'type': dict,
+     'help': 'Parameters for processing gene expression data'
+     }
+]
+
+required = None
+
+
+# experimental
+supported_definitions = ['data_url','train_data','val_data','shuffle','feature_subsample']
+
+
+class PaccmannMCA_candle(candle.Benchmark):
+
+    def set_locals(self):
+        if required is not None:
+            self.required = set(required)
+        if additional_definitions is not None:
+            self.additional_definitions = additional_definitions
+
+
+def initialize_parameters():
+    preprocessor_bmk = PaccmannMCA_candle(file_path,
+        'paccmannmca_default_model.txt',
+        'pytorch',
+        prog='PaccmannMCA_candle',
+        desc='Data Preprocessor'
+    )
+    #Initialize parameters
+    gParameters = candle.finalize_parameters(preprocessor_bmk)
+    return gParameters
+
+def preprocess(params):
+    fname='Data_MCA.zip'
+    origin=params['data_url']
+    # Download and unpack the data in CANDLE_DATA_DIR
+    candle.file_utils.get_file(fname, origin)
+
+
+def candle_main():
+    params = initialize_parameters()
+    preprocess(params)
+
+if __name__ == "__main__":
+    candle_main()
+
